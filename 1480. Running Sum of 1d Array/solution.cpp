a44d8f5c4017a60007134dd9b05f1cdb17dd98b6
pscieszka <piotr.scieszka.epinote@gmail.com>
2022-09-28 16:28:04 +0200

    .

--- 1480. Running Sum of 1d Array/solution.cpp
+++ 1480. Running Sum of 1d Array/solution.cpp
@@ -1,13 +1,35 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
 class Solution {
 public:
-    vector<int> runningSum(vector<int>& nums) {
-        vector<int> x = {nums[0]};
-        
-        for(int i=1;i<nums.size();i++)
+     int pivotIndex(vector<int>& nums) {
+        int sum=0;
+        int left=0;
+        int right;
+       for_each(nums.begin(), nums.end(), [&] (int n) {sum += n;});
+        for (int i=0; i<nums.size();i++)
         {
-            x.push_back(x[i-1]+ nums[i]);
+            right = sum -nums[i] - left;
+            if (left==right)
+            {
+                return i;
+            }
+               left+= nums[i];    
         }
-        return x;
-        
+        return -1;
     }
 };
+int main()
+{
+    Solution x;
+    vector <int> v={1,2,3};
+    cout<<x.pivotIndex(v);
+
+
+
+
+    return 0;
+}
