962736138e1eadebee25d92a1ac19bb5c00dabeb
emereks <75973678+emereks@users.noreply.github.com>
2022-11-14 20:09:46 +0100

    557. Reverse Words in a String III

---
+++ Easy/557. Reverse Words in a String III.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+string reverseWords(string s) {
+        int x = 0;
+        
+        for (int i=0; i<=s.size(); i++) {
+            if (s[i] == ' ' ){
+            reverse(s.begin()+x, s.begin()+i);
+            x = i+1;
+            }
+            else if (i == s.size()){
+            reverse(s.begin()+x, s.end());
+            }
+        }
+        
+        return s;   
+    }
+};
